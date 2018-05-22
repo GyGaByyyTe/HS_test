bfae622c8b14ada11584d169848c953e4ba873ec
GyGaByyyTe <gygabyyyte@gmail.com>
2018-05-22 10:36:31 +0300

    menu

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,7 +1,17 @@
 // menu overlay
 var sliderOverlay = $('#nav');
+var isOpen = false;
+// var body = overflow: hidden
 $('.hamburger__link').click(function(e) {
   e.preventDefault();
   $(this).toggleClass('hamburger__link--pressed');
   sliderOverlay.toggleClass('active');
+  if (isOpen) {
+    document.body.style.overflow = 'visible';
+    isOpen = false;
+  } else {
+    document.body.style.overflow = 'hidden';
+    isOpen = true;
+  }
+  console.log(isOpen);
 });
